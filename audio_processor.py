b356371fb16ec550cb92a9f21857f76ffd8c631f
zhanglichao <thegodcharles@163.com>
2024-03-28 15:35:34 +0800

    m audio_processor

--- audio_processor.py
+++ audio_processor.py
@@ -1,15 +1,32 @@
-from pydub import AudioSegment
+import pydub
+import httpx
+import io
 
 
 class AudioProcessor:
-    def __init__(self, file_path):
-        self.audio = AudioSegment.from_file(file_path)
+    def __init__(self, source_url):
+        self.content = httpx.get(source_url, verify=False).content
+        self.audio = pydub.AudioSegment.from_mp3(io.BytesIO(self.content))
 
-    def crop(self, start_ms, end_ms):
-        self.audio = self.audio[start_ms:end_ms]
+    def crop(self, start_ms, end_ms=None):
+        # 当end_ms=None开始截到末尾
+        audio_slice = slice(start_ms, end_ms)
+        self.audio = self.audio[audio_slice]
 
     def overlay(self, other_audio):
         self.audio = self.audio.overlay(other_audio)
+
+    def silent(self, duration=0, place='front'):
+        if place == 'front':
+            self.audio = pydub.AudioSegment.silent(duration=duration) + self.audio
+        else:
+            self.audio = self.audio + pydub.AudioSegment.silent(duration=duration)
+
+    def combined(self, add_audio, place='front'):
+        if place == 'front':
+            self.audio = add_audio + self.audio
+        else:
+            self.audio = self.audio + add_audio
 
     def export(self, output_path, format="mp3"):
         self.audio.export(output_path, format=format)
@@ -17,20 +34,19 @@ class AudioProcessor:
     def adjust_volume(self, db):
         self.audio = self.audio + db
 
-    def add(self, other_audio):
-        self.audio = self.audio.add(other_audio)
-
 
 # 示例用法
 if __name__ == "__main__":
     # 创建 AudioProcessor 实例
-    processor = AudioProcessor("input_audio.mp3")
+    source_url = 'https://xxxx.com'
+    processor = AudioProcessor(source_url)
 
     # 裁剪音频文件
     processor.crop(1000, 5000)
 
     # 加载要叠加的音频文件
-    overlay_audio = AudioSegment.from_file("overlay_audio.mp3")
+    add_url = 'https://yyyy.com'
+    overlay_audio = AudioProcessor(add_url)
 
     # 叠加音频文件(会将两个音频叠加在一起)
     processor.overlay(overlay_audio)
@@ -39,9 +55,5 @@ if __name__ == "__main__":
     processor.adjust_volume(3)
 
     # 导出处理后的音频文件
-    processor.export("output_audio.mp3")
-
-    # 音频连接
-    add_audio = AudioSegment.from_file("overlay_audio.mp3")
-    processor.add(add_audio)
-    
+    output_path = io.BytesIO()
+    processor.export(output_path, 'mp3')
